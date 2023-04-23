49725559f6b7430c814e87b9bb471ed0dc384c55
meSenLi <123882500+meSenLi@users.noreply.github.com>
2023-04-23 08:02:54 +0000

    finish something

--- bot.py
+++ bot.py
@@ -2,22 +2,99 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-myurl = "https://main1.phpcoin.net/apps/explorer/address.php"
-myaddress = "Pst5VEoCwbeowUoEPLc4iQQvgVKJqSJi3N"
+import datetime
 
-def fun(url, address):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'}
-    html=requests.post(url=url, headers=headers, data = {'address': address})
-    print(html)
-    return html.text
+phpurl = "https://main1.phpcoin.net/apps/explorer/"
 
-def parse(text):
-    soup = BeautifulSoup(text, 'html.parser')
-    table = soup.find('table').find_all('tbody')
-    for data in table:
-        rows = data.find_all('tr')
-        print(rows)
-        print("-----------------------------------")
 
-parse(fun(myurl, myaddress))
-    
+def pares(file):
+    def header(soup):
+        item = {}
+        for tr in soup.find_all('tr'):
+            tds = tr.find_all('td')
+            if tds[1].string:
+                value = tds[1].string
+                value = value[:value.find(' (')]
+                item[tds[0].string] = value
+            else:
+                item[tds[0].string] = tds[1].next_element
+
+            item[tds[0].string] = item[tds[0].string].replace(
+                '\n', '').replace('\r', '').strip()
+        return item
+
+    def body(soup):
+        items = []
+        keys = []
+        tr = soup.find('thead').find('tr')
+        for td in soup.find('thead').find('tr').find_all('th'):
+            keys.append(td.string)
+
+        for tr in soup.find('tbody').find_all('tr'):
+            item = {}
+            for key, td in zip(keys, tr.find_all('td')):
+                if td.string:
+                    item[key] = td.string
+                elif td.find('a') and td.find('a').find('span'):
+                    item[key] = td.find('a').find('span').string
+                elif td.next:
+                    item[key] = td.next
+                    if td.find('span'):
+                        item[key] = item[key] + td.find('span').string
+
+                else:
+                    item[key] = " "
+                item[key] = item[key].replace(
+                    '\n', '').replace('\r', '').strip()
+                item[key] = ' '.join(item[key].split())
+            items.append(item)
+        return items
+
+    soup = BeautifulSoup(file, 'html.parser')
+    table = soup.find_all('table')
+    headcontent = header(table[0])
+    bodycontent = body(table[1])
+    return [headcontent, bodycontent]
+
+
+def culclation(items):
+    now = datetime.datetime.now()
+    end = now
+    start = now - datetime.timedelta(hours=now.hour, minutes=now.minute, seconds=now.second,
+                                     microseconds=now.microsecond)
+    print(now)
+    print(start)
+    print(end)
+    income = {}
+    for item in items:
+        time = datetime.datetime.strptime(item['Date'], '%Y-%m-%d %H:%M:%S')
+        if (start <= time <= end):
+            if item['Type'] in income.keys():
+                income[item['Type']] = income[item['Type']] + \
+                    float(item['Value'])
+            else:
+                income[item['Type']] = float(item['Value'])
+    return income
+
+
+def fetch(address):
+    result = requests.get(
+        "https://main1.phpcoin.net/apps/explorer/address.php?address=" + address)
+    if result.status_code == 200:
+        return result.text
+
+
+def dailyIncom(address):
+    file = fetch(address)
+    for i in range(5):
+        if file:
+            break
+        file = fetch(address)
+        slepp(10)
+    document = pares(file)
+    income = culclation(document[1])
+    return income
+
+
+address = "Pst5VEoCwbeowUoEPLc4iQQvgVKJqSJi3N"
+print(dailyIncom(address))
--- regex.py
+++ regex.py
@@ -1,6 +1,6 @@
 import re
 
-file = "/home/lisen/workspace/phpbot/my.txt"
+file = "/workspaces/phpcoinbot/my.txt"
 
 read = open(file, mode="r")
 string = read.read()
@@ -9,28 +9,116 @@ string = read.read()
 def findAddress(string):
     res = re.findall(r'<td>Address</td>(?:.*?)<td>(.*?)<a', string, flags=re.S)
     res = res[0].replace('\n', '').replace('\r', '').strip()
-    return {'Address':res}
+    return {'Address': res}
+
 
 def findPublicKey(string):
-    res = re.findall(r'<td>Public key</td>(?:.*?)<td>(.*?)</td>', string, flags=re.S)
+    res = re.findall(
+        r'<td>Public key</td>(?:.*?)<td>(.*?)</td>', string, flags=re.S)
+    res = res[0].replace('\n', '').replace('\r', '').strip()
+    return {'Public key': res}
+
+
+def findTatalReceived(string):
+    res = re.findall(
+        r'<td>Total received</td>(?:.*?)<td>(.*?)\((?:\d*)\)</td>', string, flags=re.S)
+    res = res[0].replace('\n', '').replace('\r', '').strip()
+    return {'Total received': res}
+
+
+def findTotalSent(string):
+    res = re.findall(
+        r'<td>Total sent</td>(?:.*?)<td>(.*?)\((?:\d*)\)</td>', string, flags=re.S)
+    res = res[0].replace('\n', '').replace('\r', '').strip()
+    return {'Total received': res}
+
+
+def findBalance(string):
+    res = re.findall(
+        r'<td(?:.*?)>Balance</td>(?:.*?)<td(?:.*?)>(.*?)</td>', string, flags=re.S)
     res = res[0].replace('\n', '').replace('\r', '').strip()
     print(res)
-    return res
-
-findPublicKey(string)
+    return {'Balance': res}
 
 
+def finditems(string):
+    def retId(string):
+        res = re.findall(r'(?:.*?)title="(.*?)">(?:.*?)', string, re.S)
+        if res:
+            return res[0]
+        return ""
 
-# dict = {}
-# res = re.findall(r'<table class="table table-sm table-striped">(.*?)</table>', string,flags=re.S)
-# head = res[0]
-# # print(head)
-# for tr in re.findall(r'<tr>(.*?)</tr>', head,flags=re.S):
-#     td = re.findall(r'<td>(.*?)</td>', tr,flags=re.S)
-#     if td :
-#         dict[td[0]] = td[1]
-    
-# for it in dict.keys():
-#     print(it)
-#     print(dict[it])
+    def retDate(string):
+        res = re.findall(r'(?:.*?)>(.*?)</span>(?:.*?)', string, re.S)
+        if res:
+            return res[0]
+        return ""
 
+    def retHeight(string):
+        res = re.findall(r'(?:.*?)>(.*?)</a>(?:.*?)', string, re.S)
+        if res:
+            return res[0]
+        return ""
+
+    def retConf(string):
+        return string
+
+    def retBlock(string):
+        res = re.findall(r'(?:.*?)title="(.*?)>(?:.*?)', string, re.S)
+        if res:
+            return res[0]
+        return ""
+
+    def retFromTo(string):
+        res = re.findall(r'(?:.*?)title="(.*?)>(?:.*?)', string, re.S)
+        if res:
+            return res[0]
+        return ""
+
+    def retType(string):
+        res = re.findall(r'(?:.*?)>(.*?)</span>(?:.*?)', string, re.S)
+        if res:
+            return res[0]
+        return ""
+
+    def retValue(string):
+        # res = re.findall(r'(?:.*?)>(.*?)</span>(?:.*?)',string, re.S)
+        return string
+
+    def retFee(string):
+        res = re.findall(r'(?:.*?)>(.*?)</span>(?:.*?)', string, re.S)
+        if res:
+            return res[0]
+        return ""
+
+    items = []
+    string = re.findall(
+        r'<tbody>(.*?)</tbody>', string, flags=re.S)[0]
+    res = re.findall(r'<tr>(.*?)</tr>', string, re.S)
+
+    print(len(res))
+    for each in res:
+        item = {}
+        all = re.findall(r'<td(?:.*?)>(.*?)</td>', each, flags=re.S)
+        item['Id'] = retId(all[0]).replace('\n', '').replace('\r', '').strip()
+        item['Date'] = retDate(all[1]).replace(
+            '\n', '').replace('\r', '').strip()
+        item['Height'] = retHeight(all[2]).replace(
+            '\n', '').replace('\r', '').strip()
+        item['Conf'] = retConf(all[3]).replace(
+            '\n', '').replace('\r', '').strip()
+        item['Block'] = retBlock(all[4]).replace(
+            '\n', '').replace('\r', '').strip()
+        item['From/To'] = retFromTo(all[5]).replace('\n',
+                                                    '').replace('\r', '').strip()
+        item['Type'] = retType(all[6]).replace(
+            '\n', '').replace('\r', '').strip()
+        item['Value'] = retValue(all[7]).replace(
+            '\n', '').replace('\r', '').strip()
+        item['Fee'] = retFee(all[8]).replace(
+            '\n', '').replace('\r', '').strip()
+        items.append(item)
+    return items
+
+
+res = finditems(string)
